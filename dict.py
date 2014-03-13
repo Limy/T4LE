33affae8ec87828cb425ea3f6109e83701394d0c
Limy <Pidofme@gmail.com>
2014-03-13 17:05:35 +0800

    add global variable APIKEY

--- dict.py
+++ dict.py
@@ -6,9 +6,12 @@ import json
 import urllib
 import subprocess
 
+# api key, 1000 times per hour
+APIKEY = 'WGCxN9fzvCxPo0nqlzGLCPUc'
+
 def main():
     word = subprocess.check_output('xsel')
-    params = urllib.urlencode({'from': 'auto', 'to': 'auto', 'client_id':'WGCxN9fzvCxPo0nqlzGLCPUc', 'q': word})
+    params = urllib.urlencode({'from': 'auto', 'to': 'auto', 'client_id':APIKEY, 'q': word})
     f = urllib.urlopen("http://openapi.baidu.com/public/2.0/bmt/translate?%s", params)
     j = json.loads(f.read())
     d = dict(j['trans_result'][0])
