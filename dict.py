e43b02aa5c3ea64d24604d710818357833740ccf
Limy <Pidofme@gmail.com>
2015-06-29 21:17:01 +0800

    Generate a vocabulary list per day

--- dict.py
+++ dict.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import json
 import urllib
+import datetime
 import subprocess
 
 # api key, 1000 times per hour
 APIKEY = 'WGCxN9fzvCxPo0nqlzGLCPUc'
+PATH = '~/vocabulary' # make sure the path exist
+FILENAME = os.path.join(os.path.expanduser(PATH), str(datetime.date.today()) + '.txt')
 
 def main():
     word = subprocess.check_output('xsel')
@@ -16,6 +20,9 @@ def main():
     j = json.loads(f.read())
     d = dict(j['trans_result'][0])
     subprocess.call(['notify-send', word, d['dst']])
+
+    with open(FILENAME, 'a+', 0) as f:
+        f.write(word + '\n')
 
 if __name__ == '__main__':
     main()
