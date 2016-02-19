54d5a984aeecd9bad501ec484c173f2dc504dfa5
Limy <Pidofme@gmail.com>
2016-02-19 22:44:40 +0800

    Migrate to new translate api

--- dict.py
+++ dict.py
@@ -7,16 +7,24 @@ import json
 import urllib
 import datetime
 import subprocess
+import random
+import md5
 
-# api key, 1000 times per hour
-APIKEY = 'WGCxN9fzvCxPo0nqlzGLCPUc'
+# api key, six million per month
+APPID = 'You baidu translate appid'
+APIKEY = 'You baidu translate apikey'
 PATH = '~/vocabulary' # make sure the path exist
 FILENAME = os.path.join(os.path.expanduser(PATH), str(datetime.date.today()) + '.txt')
 
 def main():
     word = subprocess.check_output('xsel')
-    params = urllib.urlencode({'from': 'auto', 'to': 'auto', 'client_id':APIKEY, 'q': word})
-    f = urllib.urlopen("http://openapi.baidu.com/public/2.0/bmt/translate?%s", params)
+    salt = random.randint(32768, 65536)
+    sign = APPID +  word + str(salt) + APIKEY
+    m1 = md5.new()
+    m1.update(sign)
+    sign = m1.hexdigest()
+    params = urllib.urlencode({'q': word, 'from': 'auto', 'to': 'zh', 'appid':APPID, 'salt': salt, 'sign': sign})
+    f = urllib.urlopen("http://api.fanyi.baidu.com/api/trans/vip/translate?%s", params)
     j = json.loads(f.read())
     d = dict(j['trans_result'][0])
     subprocess.call(['notify-send', word, d['dst']])
